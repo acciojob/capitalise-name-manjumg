f8a840e54f4ea47af18fb6280c199e41a28d1a73
Manjunath M Goni <manjugoni007@gmail.com>
2025-05-10 11:35:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+// Ensure input is converted to uppercase when losing focus
+document.getElementById('fname').addEventListener('blur', function () {
+  this.value = this.value.toUpperCase();
+});
